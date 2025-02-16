46b64cac1297699b826d30738cfd22772cd9b62d
Hug0-Drelon <hugo.drelon@gmail.com>
2025-02-16 21:21:52 +0100

    Remove build directory from version control.

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '8d92d1052d6bcfd6bf97');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '50934a1b1e008136636a');
